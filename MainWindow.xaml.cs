8f70e7a49d7c8f24378d74bf7fb8e6543751dc22
cokee_pc <xiaoyangjunroms@outlook.com>
2022-11-06 16:35:29 +0800

    000

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -312,10 +312,14 @@ namespace CokeeDP
             bitmapImage.DownloadProgress += ImageDownloadProgress;
            bitmapImage.DownloadCompleted += DownloadImageCompleted;
             br1.Tag = dt["images"][0]["enddate"].ToString();
-            DoubleAnimation animation=  new DoubleAnimation(br1_blur.Radius,br1_blur.Radius+10,new Duration(TimeSpan.FromSeconds(3)));
-            br1.Appear();
+           
+            DoubleAnimation animation = new DoubleAnimation(0,15,new Duration(TimeSpan.FromSeconds(3)));
+             animation.EasingFunction = new CircleEase();
+            //animation.AutoReverse = true;
+            br1_blur.BeginAnimation(BlurEffect.RadiusProperty,animation);
 
             bitmapImage.BeginInit();
+            //Appear(pro,3,10,5);
             /* 
              a.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressCallback);
               a.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCallback);
@@ -327,14 +331,17 @@ namespace CokeeDP
         {
             if(pro.Visibility != Visibility.Visible) pro.Visibility = Visibility.Visible;
             pro.Value = e.Progress; 
-            log.Text = "正在加载壁纸" + e.Progress+"%"; 
+            log.Text = "LoadBitmapImage_FromWeb " + e.Progress+"% "; 
         }
 
         private void DownloadImageCompleted(object sender,EventArgs e)
         {
-            if(pro.Visibility != Visibility.Collapsed) pro.Visibility = Visibility.Collapsed;
-            log.Text = "Done.";
-            DoubleAnimation animation = new DoubleAnimation(br1_blur.Radius,br1_blur.Radius - 10,new Duration(TimeSpan.FromSeconds(10)));
+           // Disappear(pro,1,20,0.5);
+           if(pro.Visibility != Visibility.Collapsed) pro.Visibility = Visibility.Collapsed;
+            log.Text = "Image Loaded. Num:"+bing;
+            DoubleAnimation animation = new DoubleAnimation(15,0,new Duration(TimeSpan.FromSeconds(3)));
+           // animation.EasingFunction = new CircleEase();
+            //animation.AutoReverse = true;
             br1_blur.BeginAnimation(BlurEffect.RadiusProperty,animation);
             br1.Source = bitmapImage;
         }
@@ -1053,6 +1060,107 @@ namespace CokeeDP
                 ProcessErr(ex);
                 log.Text = ex.ToString();
             }
+        }
+        public void Appear(FrameworkElement element,int direction = 0,int distance = 20,double duration = .3)
+        {
+            //将所选控件的Visibility属性改为Visible，这里要首先执行否则动画看不到
+            ObjectAnimationUsingKeyFrames VisbilityAnimation = new ObjectAnimationUsingKeyFrames();
+            DiscreteObjectKeyFrame kf = new DiscreteObjectKeyFrame(Visibility.Visible,new TimeSpan(0,0,0));
+            VisbilityAnimation.KeyFrames.Add(kf);
+            element.BeginAnimation(Border.VisibilityProperty,VisbilityAnimation);
+
+            //创建新的缩放动画
+            TranslateTransform TT = new TranslateTransform();
+            element.RenderTransform = TT;
+            //创建缩放动画函数，可以自己修改
+            EasingFunctionBase easeFunction = new CircleEase() { EasingMode = EasingMode.EaseInOut };
+
+            //判断动画方向
+            if(direction == 0)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(-distance,0,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.YProperty,Animation);
+            }
+            else if(direction == 1)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(distance,0,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.XProperty,Animation);
+            }
+            else if(direction == 2)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(distance,0,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.YProperty,Animation);
+            }
+            else if(direction == 3)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(-distance,0,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.XProperty,Animation);
+            }
+            else throw new Exception("无效的方向！");
+
+            //将所选控件的可见度按动画函数方式显现
+            DoubleAnimation OpacityAnimation = new DoubleAnimation(0,1,new Duration(TimeSpan.FromSeconds(duration)));
+            OpacityAnimation.EasingFunction = easeFunction;
+            element.BeginAnimation(Border.OpacityProperty,OpacityAnimation);
+        }
+
+        /// <summary>
+        /// 淡出动画(控件名, 0：上方；1：右方；2：下方；3：左方, 淡出的距离，持续时间)
+        /// </summary>
+        /// <param name="element">控件名</param>
+        /// <param name="direction">0：上方；1：右方；2：下方；3：左方</param>
+        /// <param name="distance">淡出的距离</param>
+        /// <param name="duration">持续时间</param>
+        public void Disappear(FrameworkElement element,int direction = 0,int distance = 20,double duration = .3)
+        {
+            //创建新的缩放动画
+            TranslateTransform TT = new TranslateTransform();
+            element.RenderTransform = TT;
+            //创建缩放动画函数，可以自己修改
+            EasingFunctionBase easeFunction = new CircleEase() { EasingMode = EasingMode.EaseInOut };
+
+            //判断动画方向
+            if(direction == 0)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(-distance,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.YProperty,Animation);
+            }
+            else if(direction == 1)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(distance,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.XProperty,Animation);
+            }
+            else if(direction == 2)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(distance,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.YProperty,Animation);
+            }
+            else if(direction == 3)
+            {
+                DoubleAnimation Animation = new DoubleAnimation(-distance,new Duration(TimeSpan.FromSeconds(duration)));
+                Animation.EasingFunction = easeFunction;
+                element.RenderTransform.BeginAnimation(TranslateTransform.XProperty,Animation);
+            }
+            else
+                throw new Exception("无效的方向！");
+
+            //将所选控件的可见度按动画函数方式消失
+            DoubleAnimation OpacityAnimation = new DoubleAnimation(1,0,new Duration(TimeSpan.FromSeconds(duration)));
+            OpacityAnimation.EasingFunction = easeFunction;
+            element.BeginAnimation(Border.OpacityProperty,OpacityAnimation);
+
+            //将所选控件的Visibility属性改为Collapsed，这样不占用空间
+            ObjectAnimationUsingKeyFrames VisbilityAnimation = new ObjectAnimationUsingKeyFrames();
+            DiscreteObjectKeyFrame kf = new DiscreteObjectKeyFrame(Visibility.Collapsed,new TimeSpan(0,0,1));
+            VisbilityAnimation.KeyFrames.Add(kf);
+            element.BeginAnimation(Border.VisibilityProperty,VisbilityAnimation);
         }
     }
 }
